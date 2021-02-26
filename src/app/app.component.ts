8f19b0a5cc64d5b4493ac33bc5c78277f3e8ce2b
MichielVDD <michvdd@hotmail.com>
2021-02-26 15:33:59 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { TreeNode } from "primeng/api";
+import { Table } from "primeng/table";
 
 @Component({
   selector: "app-root",
-  templateUrl: "./app.component.html"
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  files2: TreeNode[] = <TreeNode[]>[
+export class AppComponent implements OnInit {
+  doelstellingboom: TreeNode[] = <TreeNode[]>[
     {
       label: "SD: Vrijetijdsbesteding",
       children: [
@@ -29,6 +31,7 @@ export class AppComponent {
 
   beleidsItems = [
     {
+      id: 1000,
       beleidselement: "SE2.2.5 Herstelling kunstgras hockeyveld",
       looptijd: "2020-2021",
       prioritair: true,
@@ -38,91 +41,46 @@ export class AppComponent {
       opvolger: "Jan Peeters",
       tag: "SPORT;HEIST",
       indicator: "Stand van zaken",
-      takenlijst: ""
+      takenlijst: "",
+      auteur: "Michiel"
     },
     {
-      beleidselement: "SA6.2.8 Implementatie nieuwe huisstijl",
-      looptijd: "2020-2021",
+      id: 1001,
+      beleidselement: "SE6.2.8 Implementatie nieuwe huisstijl",
+      looptijd: "2020-2025",
       prioritair: true,
       status: "",
-      verantwoordelijke:
-        "Michiel Van Der Donck; Michel Lochten; Margarita Dubkova",
-      opvolger: "Jan Peeters",
-      tag: "COMMUNICATIE",
-      indicator: "Geen indicator",
-      takenlijst: ""
-    },
-    {
-      beleidselement: "SE2.2.5 Herstelling kunstgras hockeyveld",
-      looptijd: "2020-2021",
-      prioritair: true,
-      status: "",
-      verantwoordelijke:
-        "Michiel Van Der Donck; Michel Lochten; Margarita Dubkova",
-      opvolger: "Jan Peeters",
-      tag: "SPORT;HEIST",
-      indicator: "Stand van zaken",
-      takenlijst: ""
-    },
-    {
-      beleidselement: "SA6.2.8 Implementatie nieuwe huisstijl",
-      looptijd: "2020-2021",
-      prioritair: true,
-      status: "",
-      verantwoordelijke:
-        "Michiel Van Der Donck; Michel Lochten; Margarita Dubkova",
-      opvolger: "Jan Peeters",
-      tag: "COMMUNICATIE",
-      indicator: "Geen indicator",
-      takenlijst: ""
-    },
-    {
-      beleidselement: "SE2.2.5 Herstelling kunstgras hockeyveld",
-      looptijd: "2020-2021",
-      prioritair: true,
-      status: "",
-      verantwoordelijke:
-        "Michiel Van Der Donck; Michel Lochten; Margarita Dubkova",
-      opvolger: "Jan Peeters",
-      tag: "SPORT;HEIST",
-      indicator: "Stand van zaken",
-      takenlijst: ""
-    },
-    {
-      beleidselement: "SA6.2.8 Implementatie nieuwe huisstijl",
-      looptijd: "2020-2021",
-      prioritair: true,
-      status: "",
-      verantwoordelijke:
-        "Michiel Van Der Donck; Michel Lochten; Margarita Dubkova",
-      opvolger: "Jan Peeters",
-      tag: "COMMUNICATIE",
-      indicator: "Geen indicator",
-      takenlijst: ""
-    },
-    {
-      beleidselement: "SE2.2.5 Herstelling kunstgras hockeyveld",
-      looptijd: "2020-2021",
-      prioritair: true,
-      status: "",
-      verantwoordelijke:
-        "Michiel Van Der Donck; Michel Lochten; Margarita Dubkova",
-      opvolger: "Jan Peeters",
-      tag: "SPORT;HEIST",
-      indicator: "Stand van zaken",
-      takenlijst: ""
-    },
-    {
-      beleidselement: "SA6.2.8 Implementatie nieuwe huisstijl",
-      looptijd: "2020-2021",
-      prioritair: true,
-      status: "",
-      verantwoordelijke:
-        "Michiel Van Der Donck; Michel Lochten; Margarita Dubkova",
-      opvolger: "Jan Peeters",
-      tag: "COMMUNICATIE",
-      indicator: "Geen indicator",
-      takenlijst: ""
+      verantwoordelijke: "Jan Peeters",
+      opvolger: "Sophie Verheecke",
+      tag: "HEIST",
+      indicator: "Aantal volgers op facebook",
+      takenlijst: "",
+      auteur: "Sophie"
     }
   ];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.expandAll();
+  }
+
+  expandAll() {
+    this.doelstellingboom.forEach(node => {
+      this.expandRecursive(node, true);
+    });
+  }
+
+  private expandRecursive(node: TreeNode, isExpand: boolean) {
+    node.expanded = isExpand;
+    if (node.children) {
+      node.children.forEach(childNode => {
+        this.expandRecursive(childNode, isExpand);
+      });
+    }
+  }
+
+  clear(table: Table) {
+    table.clear();
+  }
 }
